092fc16f5e7460c61da424f5690bee157cfb7099
Zack289 <ozack911@gmail.com>
2026-05-02 16:33:10 +0545

    implement the required fonts for the project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,14 +2,17 @@ import "@/global.css";
 import { Text } from "react-native";
 import { styled } from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
 
 const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold text-primary">Home</Text>
+
+      <Link href='/onboarding' className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Onboarding</Link>
+      <Link href='/(auth)/signIn' className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Onboarding</Link>
+      <Link href='/(auth)/signUp' className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Onboarding</Link>
     </SafeAreaView>
   );
 }
