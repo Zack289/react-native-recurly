cee984adc5cf057d3677de6c3e35fb7f15db28da
Zack289 <ozack911@gmail.com>
2026-05-04 22:26:43 +0545

    chore: update dependencies to include @clerk/expo and expo-secure-store for the auth implementataion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
-import { SplashScreen, Stack } from "expo-router";
 import "@/global.css";
 import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+
 SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -22,5 +31,9 @@ export default function RootLayout() {
 
     if (!fontsLoaded) return;
   }, [fontsLoaded]);
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ClerkProvider>
+  );
 }
