52d4c493630a04286994c6a473ad3e98d6ea1c45
Zack289 <ozack911@gmail.com>
2026-05-07 19:07:11 +0545

    ready to build

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 // app.config.js – extends app.json with runtime extras read from .env
 // PostHog keys are injected via process.env (loaded by Expo CLI from .env)
 // and accessed in app code via expo-constants: Constants.expoConfig?.extra
-const baseConfig = require('./app.json')
+const baseConfig = require("./app.json");
 
 module.exports = {
   expo: {
@@ -9,6 +9,9 @@ module.exports = {
     extra: {
       posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
       posthogHost: process.env.POSTHOG_HOST,
+      eas: {
+        projectId: "0ab26989-ce0b-405e-83e6-05601de6ff0a",
+      },
     },
   },
-}
+};
